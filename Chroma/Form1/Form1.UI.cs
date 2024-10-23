3dd750156b66612756cd9538a8509efcf4aa1c79
trungtaxp <phamtrungta2001@gmail.com>
2024-10-23 13:54:39 +0700

    [wip] update view form

--- Chroma/Form1/Form1.UI.cs
+++ Chroma/Form1/Form1.UI.cs
@@ -11,11 +11,38 @@ namespace Chroma.Form1
         private GroupBox _keithleyGroupBox;
         private GroupBox _chromaGroupBox;
         private GroupBox _functionGroupBox;
+        private ComboBox _controlComboBox;
 
         private void InitializeCustomComponents()
         {
             this.BackColor = Color.White;
 
+            // Create MenuStrip
+            var menuStrip = new MenuStrip { Dock = DockStyle.Top };
+
+            // Create File menu
+            var fileMenu = new ToolStripMenuItem("File");
+            fileMenu.DropDownItems.Add("Exit", null, (s, e) => { this.Close(); });
+
+            // Create Edit menu
+            var editMenu = new ToolStripMenuItem("Options");
+            editMenu.DropDownItems.Add("Settings", null, (s, e) => {ShowSettingsDialog(); });
+
+            // Create View menu
+            var viewMenu = new ToolStripMenuItem("View");
+            viewMenu.DropDownItems.Add("Zoom In", null, (s, e) => { /* Zoom In action */ });
+            viewMenu.DropDownItems.Add("Zoom Out", null, (s, e) => { /* Zoom Out action */ });
+
+            // Add menus to MenuStrip
+            menuStrip.Items.Add(fileMenu);
+            menuStrip.Items.Add(editMenu);
+            menuStrip.Items.Add(viewMenu);
+
+            // Add MenuStrip to the form
+            this.MainMenuStrip = menuStrip;
+            this.Controls.Add(menuStrip);
+
+            // Existing SplitContainer code
             _mainSplitContainer = new SplitContainer
             {
                 Dock = DockStyle.Fill,
@@ -24,6 +51,7 @@ namespace Chroma.Form1
                 IsSplitterFixed = true // Prevent moving the splitter
             };
             this.Controls.Add(_mainSplitContainer);
+            this.Controls.SetChildIndex(menuStrip, 15); // Ensure MenuStrip is on top
 
             _secondarySplitContainer = new SplitContainer
             {
@@ -65,5 +93,26 @@ namespace Chroma.Form1
             this.FormBorderStyle = FormBorderStyle.Sizable;
             this.WindowState = FormWindowState.Maximized;
         }
+
+        private void ShowSettingsDialog()
+        {
+            _controlComboBox = new ComboBox
+            {
+                Name = "Setting Device",
+                DropDownStyle = ComboBoxStyle.DropDownList,
+                Dock = DockStyle.Top,
+                Location = new Point(10, 10) // Adjust the location as needed
+            };
+
+            var comboBoxForm = new Form
+            {
+                Text = "Setting",
+                Size = new Size(855, 550)
+            };
+
+            comboBoxForm.Controls.Add(_controlComboBox);
+            comboBoxForm.ShowDialog();
+        }
+        
     }
 }
